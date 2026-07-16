350b4b0b26792e70c8aa3b0c9da29c2bd2e6a73f
Karthigashri R <shri7122005@gmail.com.com>
2026-07-16 18:23:42 +0530

    Add Week 5 React Handson

---
+++ Week 5/React/4_ReactJS-HOL/blogapp/src/Posts.js
@@ -0,0 +1,81 @@
+import React from "react";
+
+class Posts extends React.Component {
+
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+            posts: []
+        };
+
+    }
+
+    loadPosts() {
+
+        fetch("https://jsonplaceholder.typicode.com/posts")
+
+            .then(response => response.json())
+
+            .then(data => {
+
+                this.setState({
+
+                    posts: data
+
+                });
+
+            });
+
+    }
+
+    componentDidMount() {
+
+        this.loadPosts();
+
+    }
+
+    componentDidCatch(error, info) {
+
+        alert(error);
+
+        console.log(info);
+
+    }
+
+    render() {
+
+        return (
+
+            <div>
+
+                <h1>Posts</h1>
+
+                {
+
+                    this.state.posts.map(post =>
+
+                        <div key={post.id}>
+
+                            <h3>{post.title}</h3>
+
+                            <p>{post.body}</p>
+
+                            <hr />
+
+                        </div>
+
+                    )
+
+                }
+
+            </div>
+
+        );
+
+    }
+
+}
+
+export default Posts;
